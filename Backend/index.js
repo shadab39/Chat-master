8cee5e01d55c7a8412704a8f79d225979794db5d
Mohd Shadab <amdshadab746@example.com>
2026-07-22 22:46:52 +0530

    Test CORS

--- Backend/index.js
+++ Backend/index.js
@@ -15,14 +15,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",
-      "https://chat-master-iota.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
 );
 
 // Routes
